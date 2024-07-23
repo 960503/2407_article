929ef20d78825dc8db4dc994e83a7bd13f66908e
960503 <hanewnji@gmail.com>
2024-07-23 19:24:54 +0900

    게시판 틀 잡기

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+class Main{
+	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("명령어 ) ");
+		String cmd = sc.nextLine();
+		
+		
+		System.out.println("== 프로그램 끝 ==");
+	}
+}
